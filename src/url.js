cf36c7c547192e46a5999cf83c88658710c1b68e
Franklin M <franklinmuriithi431@gmail.com>
2024-06-16 21:58:40 +0300

    new image

--- src/url.js
+++ src/url.js
@@ -2,4 +2,4 @@
 export const IF = import.meta.env.VITE_IF */
 
 export const URL = "https://dev-blog1-backend-1.onrender.com"
-export const IF = "https://dev-blog1-backend-1.onrender.com/images"
+export const IF = "https://dev-blog1-backend-1.onrender.com/images/"
