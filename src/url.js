7d3b10b12954761bd0ec4487c28c2cc3a7e42be5
Franklin M <franklinmuriithi431@gmail.com>
2024-06-16 21:33:31 +0300

    jina

--- src/url.js
+++ src/url.js
@@ -1,2 +1,5 @@
-export const URL = import.meta.env.VITE_URL
-export const IF = import.meta.env.VITE_IF
+/* export const URL = import.meta.env.VITE_URL
+export const IF = import.meta.env.VITE_IF */
+
+export const URL = "https://dev-blog1-backend-1.onrender.com"
+export const IF = "https://dev-blog1-backend-1.onrender.com/images"
